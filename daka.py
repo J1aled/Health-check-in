88888b0e86fdc3efb13b2491fec6cd6102989876
howdy-king <44528383+howdy-king@users.noreply.github.com>
2020-10-30 00:41:28 +0800

    Update daka.py

--- daka.py
+++ daka.py
@@ -2,6 +2,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 class user(object):
     def __init__(self,uid,pwd):
@@ -20,6 +23,7 @@ class user(object):
             browser.find_element_by_name('txtMM').send_keys(self.pwd)
             browser.find_element_by_id('IbtnEnter').click()
             time.sleep(0.5)
+
             t = time.strftime('%m%d',time.localtime())
             browser.find_element_by_link_text(t+'疫情防控——师生健康状态采集').click()
             time.sleep(0.5)
@@ -36,19 +40,51 @@ class user(object):
             s5.select_by_value("1")
             browser.find_element_by_name("B2").click()
             print(t+'打卡成功！')
+            global flag
+            flag = True
 
         except Exception as e:
             print("there is an exception:" + str(e))
             print('打卡失败')
         finally:
             browser.quit()
+
+def send_email():
+    from_addr = "GHJKL777@126.com"
+    password = 'BCLLVAOISZZYGEWK'
+
+    to_addr = "1971346995@qq.com"
+    t = time.strftime('%m月%d日',time.localtime())
+    mail_text = t+"健康打卡打卡成功！"
+
+    msg = MIMEText(mail_text)
+    msg['Subject'] = "每日健康打卡通知"
+    msg['From'] = Header(from_addr)
+    msg['To'] = Header(to_addr)
+
+    try:
+        server = smtplib.SMTP_SSL("smtp.126.com",465)
+        server.login(from_addr,password)
+        server.sendmail(from_addr,to_addr,msg.as_string())
+        server.quit()
+        print("邮件发送成功")
+    except Exception as e:
+        print("Error: 无法发送邮件" + str(e))
+
+
 def main():
+    global flag
+    flag = False
     u1 = user("2018122017","GJL053132a")
     u1.sign_in()
     time.sleep(1)
     u2 = user("2017121162","GJJ040200a")
     u2.sign_in()
-    print("大功告成！！！")
+    if flag == True:
+        send_email()
+    else:
+        print("wrong")
+
 
 if __name__ == "__main__":
     main()
